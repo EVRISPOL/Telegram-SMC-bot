ac47e7860bcd1cef58318195f62c7c19dcc64d3c
EVRISPOL <evris.1914@gmail.com>
2025-04-19 00:04:24 +0300

    Delete ohlc.py

--- ohlc.py
+++
@@ -1,8 +0,0 @@
-import ccxt
-import pandas as pd
-
-def fetch_ohlc(symbol="BTC/USDT", timeframe="3m", limit=100):
-    exchange = ccxt.binance()
-    ohlc = exchange.fetch_ohlcv(symbol, timeframe, limit=limit)
-    df = pd.DataFrame(ohlc, columns=["timestamp", "open", "high", "low", "close", "volume"])
-    return df
