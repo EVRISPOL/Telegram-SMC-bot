a52a267a021f7325820da0b143562cb58fb1253e
EVRISPOL <evris.1914@gmail.com>
2025-05-09 22:24:25 +0300

    Add files via upload

---
+++ handlers/analyze_handler.py
@@ -0,0 +1,7 @@
+
+from telegram import Update
+from telegram.ext import ContextTypes
+
+async def analyze(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # Εδώ θα υλοποιηθεί η λογική για την εντολή /analyze
+    pass
---
+++ handlers/autosignal_handler.py
@@ -0,0 +1,7 @@
+
+from telegram import Update
+from telegram.ext import ContextTypes
+
+async def autosignal(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # Εδώ θα υλοποιηθεί η λογική για την εντολή /autosignal
+    pass
