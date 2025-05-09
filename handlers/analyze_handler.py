8921a2a6c5ae4e67c517c93118b6e97477c4e22d
EVRISPOL <evris.1914@gmail.com>
2025-05-09 22:46:15 +0300

    Update analyze_handler.py

--- handlers/analyze_handler.py
+++ handlers/analyze_handler.py
@@ -1,6 +1,6 @@
-
 from telegram import Update
 from telegram.ext import ContextTypes
+from binance_utils import get_klines #για την ληψη των candlesticks 
 
 async def analyze(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # Εδώ θα υλοποιηθεί η λογική για την εντολή /analyze
