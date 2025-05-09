6ee64a293f4837241ba59f0bd887c0e643145535
EVRISPOL <evris.1914@gmail.com>
2025-05-09 22:50:26 +0300

    Update analyze_handler.py

--- handlers/analyze_handler.py
+++ handlers/analyze_handler.py
@@ -1,6 +1,6 @@
 from telegram import Update
 from telegram.ext import ContextTypes
-from binance_utils import get_klines #για την ληψη των candlesticks 
+from binance_utils import get_klines #για την ληψη των candlesticks #### Η get_klines() είναι μια Python συνάρτηση που δημιουργήσαμε για να τραβάει ιστορικά candlesticks (κεριά) από το Binance API.
 
 async def analyze(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # Εδώ θα υλοποιηθεί η λογική για την εντολή /analyze
