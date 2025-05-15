da59c4051f235e611a348d2cd4446ac4a6bc064c
EVRISPOL <evris.1914@gmail.com>
2025-05-16 00:28:56 +0300

    Update win_percent.py

--- analyze_module/win_percent.py
+++ analyze_module/win_percent.py
@@ -1,3 +1,5 @@
+# Βάρη ανά δείκτη για υπολογισμό WIN %
+# Συνάρτηση που υπολογίζει το ποσοστό επιτυχίας βάσει επιβεβαιώσεων
 
 def calculate_win_percent(indicators, signal, mtf_result=True):
     adx = indicators['adx']
